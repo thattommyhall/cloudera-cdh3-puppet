a3c56991d1d31489ca7f41a6a3e40be06aaa1f9c
thattommyhall <thattommyhall@gmail.com>
2012-12-20 16:31:08 +0000

    added some new facts

--- modules/hadoop/lib/facter/facts.rb
+++ modules/hadoop/lib/facter/facts.rb
@@ -1,7 +1,21 @@
 Facter.add("hadoop_hdfs_data_dirs") do
-  setcode { Dir.glob('/data*/hdfs/data/').join(',') }
+  path = Dir.glob('/data*/hdfs/data/').join(',')
+  setcode { path.empty? ? "/tmp/hadoop" : path }
 end
 
 Facter.add("hadoop_mapred_local_dirs") do
-  setcode { Dir.glob('/data*/mapred/local/').join(',') }
+  path = Dir.glob('/data*/mapred/local/').join(',')
+  setcode { path.empty? ? "/tmp/hadoop" : path }
+end
+
+mapper_ratio = 0.8
+oversubscription = 0.2
+total_slots = (Facter.processorcount.to_i * (1+oversubscription)).ceil
+
+Facter.add("mapred_tasktracker_reduce_tasks_maximum") do
+  setcode { (total_slots * (1-mapper_ratio)).ceil }
+end
+
+Facter.add("mapred_tasktracker_map_tasks_maximum") do
+  setcode { (total_slots * mapper_ratio).ceil }
 end
